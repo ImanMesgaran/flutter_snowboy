dfe53a286c30540b3ee16230f13655960f6453f4
ImanMesgaran <aic_sis@yahoo.com>
2021-11-18 09:58:02 +0330

    implementing custom model.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -27,6 +27,7 @@ import 'package:flutter_snowboy/flutter_snowboy.dart';
 import 'package:audiofileplayer/audiofileplayer.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter_sound_lite/flutter_sound.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(SnowboyExampleApp());
@@ -55,9 +56,14 @@ class _SnowboyExampleAppState extends State<SnowboyExampleApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
+    PermissionStatus status = await Permission.microphone.request();
+    if (status != PermissionStatus.granted)
+      throw RecordingPermissionException("Microphone permission not granted");
+
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
-      String modelPath = await copyModelToFilesystem("hi_embla.pmdl");
+      //String modelPath = await copyModelToFilesystem("hi_embla.pmdl");
+      String modelPath = await copyModelToFilesystem("hey_tulu.pmdl");
       // Create detector object and prepare it
       detector = Snowboy();
       await detector.prepare(modelPath);
@@ -74,7 +80,8 @@ class _SnowboyExampleAppState extends State<SnowboyExampleApp> {
     }
     ByteData bytes = await rootBundle.load("assets/$filename");
     final buffer = bytes.buffer;
-    File(finalPath).writeAsBytes(buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes));
+    File(finalPath).writeAsBytes(
+        buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes));
     return finalPath;
   }
 
@@ -96,7 +103,8 @@ class _SnowboyExampleAppState extends State<SnowboyExampleApp> {
 
     // Create recording stream
     _recordingDataController = StreamController<Food>();
-    _recordingDataSubscription = _recordingDataController.stream.listen((buffer) {
+    _recordingDataSubscription =
+        _recordingDataController.stream.listen((buffer) {
       // When we get data, feed it into Snowboy detector
       if (buffer is FoodData) {
         Uint8List copy = new Uint8List.fromList(buffer.data);
@@ -127,10 +135,10 @@ class _SnowboyExampleAppState extends State<SnowboyExampleApp> {
 
     if (running == false) {
       startDetection();
-      s = "Snowboy is running\nSay 'Hi Embla' to trigger hotword handler.";
+      //s = "Snowboy is running\nSay 'Hi Embla' to trigger hotword handler.";
+      s = "Snowboy is running\nSay 'Hey Tulu' to trigger hotword handler.";
       t = "Stop detection";
       r = true;
-
     } else {
       stopDetection();
       s = "Snowboy is not running";
